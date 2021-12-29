018aa0daaa61b7e3ea55a25f99062eadf4e9312f
Muhammed <muhammad709059952@gmail.com>
2021-12-29 07:35:18 +0300

    Initial commit

---
+++ KaldirJavaOrnek/src/upcasting/PayrollOffice.java
@@ -0,0 +1,10 @@
+package upcasting;
+
+public class PayrollOffice {
+
+	public void paySalary(Employee employee) {
+
+		double salary = employee.calculateSalary();
+		System.out.println("Paying a salary of " + salary + " to " + employee.getName());
+	}
+}
---
+++ src/ch1/SelamTest.java
@@ -0,0 +1,12 @@
+package ch1;
+
+public class SelamTest {
+
+	public static void main(String[] args) {
+		Selam nesne =new Selam();
+		String cevap = nesne.selamSoyle("Ali");
+		System.out.println(cevap);
+
+	}
+
+}
